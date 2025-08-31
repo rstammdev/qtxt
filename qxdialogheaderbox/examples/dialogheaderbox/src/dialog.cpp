22f2643150b3d487592c841457af486110505c33
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-31 12:18:43 +0200

    Add layout

--- qxdialogheaderbox/examples/dialogheaderbox/src/dialog.cpp
+++ qxdialogheaderbox/examples/dialogheaderbox/src/dialog.cpp
@@ -8,9 +8,19 @@
 
 #include "dialog.h"
 
+#include <QVBoxLayout>
+
+using namespace Qt::Literals::StringLiterals;
+
 
 Dialog::Dialog(QWidget* parent)
     : QDialog{parent}
 {
+    //
 
+    QVBoxLayout* layout = new QVBoxLayout;
+    setLayout(layout);
+
+    setWindowTitle("Example of QxDialogHeaderBox"_L1);
+    setMinimumSize(320, 240);
 }
