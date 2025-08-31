50a6ee0098695ba716378a7176a0e2a98f762224
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-31 12:29:10 +0200

    Add QxDialogHeaderBox

--- qxdialogheaderbox/examples/dialogheaderbox/src/dialog.cpp
+++ qxdialogheaderbox/examples/dialogheaderbox/src/dialog.cpp
@@ -11,12 +11,22 @@
 #include <QDialogButtonBox>
 #include <QVBoxLayout>
 
+#include <qxdialogheaderbox.h>
+
 using namespace Qt::Literals::StringLiterals;
 
 
 Dialog::Dialog(QWidget* parent)
     : QDialog{parent}
 {
+    // Header
+
+    QxDialogHeaderBox* headerBox = new QxDialogHeaderBox(this);
+    headerBox->setTitle("DialogHeaderBox"_L1);
+    headerBox->setSubTitle("v1.2.3"_L1);
+    headerBox->setDescription("This is an example of QxDialogHeaderBox in action."_L1);
+    headerBox->setIcon(QIcon::fromTheme(QIcon::ThemeIcon::DialogInformation));
+
     // Button
 
     QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Close);
@@ -26,6 +36,7 @@ Dialog::Dialog(QWidget* parent)
     //
 
     QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget(headerBox);
     layout->addStretch();
     layout->addWidget(buttonBox);
     setLayout(layout);
