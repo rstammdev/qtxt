89169006a88b47480224767deabc631ed9fe5fa5
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-09-02 11:22:33 +0200

    Add button 'Close'

--- qxconfirmationbox/examples/confirmationbox/src/dialog.cpp
+++ qxconfirmationbox/examples/confirmationbox/src/dialog.cpp
@@ -8,12 +8,28 @@
 
 #include "dialog.h"
 
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+
 using namespace Qt::Literals::StringLiterals;
 
 
 Dialog::Dialog(QWidget* parent)
     : QDialog{parent}
 {
+
+    // Button
+
+    QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Close);
+
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &Dialog::close);
+
+    //
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addStretch();
+    layout->addWidget(buttonBox);
+    setLayout(layout);
 
     setWindowTitle("Example of QxConfirmationBox"_L1);
     setMinimumSize(320, 240);
