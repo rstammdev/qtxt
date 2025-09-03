3bf9e845d6d726145456c4cfef7a9e8a3427ee58
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-09-03 10:49:09 +0200

    Add button 'Close'

--- qxcomponentsdialog/examples/componentsdialog/src/dialog.cpp
+++ qxcomponentsdialog/examples/componentsdialog/src/dialog.cpp
@@ -10,10 +10,27 @@
 
 using namespace Qt::Literals::StringLiterals;
 
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+
 
 Dialog::Dialog(QWidget* parent)
     : QDialog{parent}
 {
+
+    // Button
+
+    QDialogButtonBox* buttonBox = new QDialogButtonBox;
+    buttonBox->addButton(QDialogButtonBox::Close);
+
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &Dialog::close);
+
+    //
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addStretch();
+    layout->addWidget(buttonBox);
+    setLayout(layout);
 
     setWindowTitle("Example of QxComponentsDialog"_L1);
     setMinimumSize(320, 240);
