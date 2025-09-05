1bc7c329e2c7d2c25a811770cfa6c42f7528e67f
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-09-05 14:24:14 +0200

    Add button 'Cancel'

--- qxtoolbarsdialog/src/qxtoolbarsdialog.cpp
+++ qxtoolbarsdialog/src/qxtoolbarsdialog.cpp
@@ -8,10 +8,27 @@
 
 #include "qxtoolbarsdialog.h"
 
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+
 
 QxToolbarsDialog::QxToolbarsDialog(QWidget* parent)
     : QDialog{parent}
 {
+
+    // Buttons
+
+    QDialogButtonBox* buttonBox = new QDialogButtonBox;
+    buttonBox->addButton(QDialogButtonBox::Cancel);
+
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &QxToolbarsDialog::close);
+
+    //
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addStretch();
+    layout->addWidget(buttonBox);
+    setLayout(layout);
 
     setWindowTitle(tr("Configure Toolbars"));
     setMinimumSize(1024, 576);
