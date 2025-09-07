8ce078fa5d454ed68d13554d5bc5dc6c8da14786
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-09-07 19:29:02 +0200

    Display page title

--- qxtoolbarsdialog/src/qxtoolbarstoolbarpageseparator.cpp
+++ qxtoolbarsdialog/src/qxtoolbarstoolbarpageseparator.cpp
@@ -8,12 +8,33 @@
 
 #include "qxtoolbarstoolbarpageseparator.h"
 
+#include <QFont>
+#include <QLabel>
+#include <QVBoxLayout>
+
 
 QxToolbarsToolbarPageSeparator::QxToolbarsToolbarPageSeparator(QAction* separator, QWidget* parent)
     : QxToolbarsToolbarPage{parent}
     , m_separator{separator}
 {
+    QLabel* pageTitle = new QLabel;
 
+    connect(this, &QxToolbarsToolbarPage::pageTitleChanged, pageTitle, &QLabel::setText);
+
+
+
+    //
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->setContentsMargins(-1, -1, 0, 0);
+    layout->addWidget(pageTitle);
+    layout->addStretch();
+    setLayout(layout);
+
+    QFont fontTitle = pageTitle->font();
+    fontTitle.setPointSize(int(fontTitle.pointSizeF() * 1.2));
+    fontTitle.setBold(true);
+    pageTitle->setFont(fontTitle);
 }
 
 
