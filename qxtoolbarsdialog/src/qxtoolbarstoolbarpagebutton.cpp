28880fff6864a62a0ca3fd1e94b1859cc9f49950
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-09-07 18:47:54 +0200

    Display page title

--- qxtoolbarsdialog/src/qxtoolbarstoolbarpagebutton.cpp
+++ qxtoolbarsdialog/src/qxtoolbarstoolbarpagebutton.cpp
@@ -8,12 +8,33 @@
 
 #include "qxtoolbarstoolbarpagebutton.h"
 
+#include <QFont>
+#include <QLabel>
+#include <QVBoxLayout>
+
 
 QxToolbarsToolbarPageButton::QxToolbarsToolbarPageButton(QToolButton* button, QWidget* parent)
     : QxToolbarsToolbarPage{parent}
     , m_button{button}
 {
+    QLabel* pageTitle = new QLabel;
 
+    connect(this, &QxToolbarsToolbarPage::pageTitleChanged, pageTitle, &QLabel::setText);
+
+
+
+    //
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->setContentsMargins(-1, -1, 0, 0);
+    layout->addWidget(pageTitle);
+    layout->addStretch();
+    setLayout(layout);
+
+    QFont fontTitle = pageTitle->font();
+    fontTitle.setPointSize(int(fontTitle.pointSizeF() * 1.2));
+    fontTitle.setBold(true);
+    pageTitle->setFont(fontTitle);
 }
 
 
