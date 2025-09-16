3a4bc42ba08263d45d1dbf80fc5463eca65426d7
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-09-17 00:39:21 +0200

    Add command 'Quit'

--- qxthemebutton/examples/themebutton/src/mainwindow.cpp
+++ qxthemebutton/examples/themebutton/src/mainwindow.cpp
@@ -8,6 +8,7 @@
 
 #include "mainwindow.h"
 
+#include <QAction>
 #include <QMenuBar>
 #include <QToolBar>
 
@@ -17,12 +18,19 @@ using namespace Qt::Literals::StringLiterals;
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
 {
+    QAction* actionQuit = addAction(tr("&Quit"));
+    actionQuit->setObjectName("actionQuit"_L1);
+    actionQuit->setIcon(QIcon::fromTheme(QIcon::ThemeIcon::ApplicationExit));
 
     QMenu* menuFile = menuBar()->addMenu(tr("&File"));
     menuFile->setObjectName("menuFile"_L1);
+    menuFile->addAction(actionQuit);
 
     QToolBar* toolbarFile = addToolBar(tr("File Toolbar"));
     toolbarFile->setObjectName("toolbarFile"_L1);
+    toolbarFile->addAction(actionQuit);
+
+    connect(actionQuit, &QAction::triggered, this, &MainWindow::close);
 
     setMinimumSize(640, 480);
 }
