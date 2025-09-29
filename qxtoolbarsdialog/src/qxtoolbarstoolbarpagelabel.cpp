52dd793e71e205e9a12b50c0cb99daa15c8e4b0f
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-09-29 13:16:08 +0200

    Display page title

--- qxtoolbarsdialog/src/qxtoolbarstoolbarpagelabel.cpp
+++ qxtoolbarsdialog/src/qxtoolbarstoolbarpagelabel.cpp
@@ -8,10 +8,24 @@
 
 #include "qxtoolbarstoolbarpagelabel.h"
 
+#include <QVBoxLayout>
+
+#include <qxheadinglabel.h>
+
 
 QxToolbarsToolbarPageLabel::QxToolbarsToolbarPageLabel(QLabel* label, QWidget* parent)
     : QxToolbarsToolbarPage{parent}
     , m_label{label}
 {
+    QxHeadingLabel* pageTitle = new QxHeadingLabel;
 
+    connect(this, &QxToolbarsToolbarPage::pageTitleChanged, pageTitle, &QxHeadingLabel::setText);
+
+    //
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->setContentsMargins(-1, -1, 0, 0);
+    layout->addWidget(pageTitle);
+    layout->addStretch();
+    setLayout(layout);
 }
