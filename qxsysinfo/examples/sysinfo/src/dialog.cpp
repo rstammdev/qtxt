e08e9f193479cf0b27350fbdbfeba1eea5572955
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-10-15 17:49:56 +0200

    Display 'Build Qt Version'

--- qxsysinfo/examples/sysinfo/src/dialog.cpp
+++ qxsysinfo/examples/sysinfo/src/dialog.cpp
@@ -9,12 +9,22 @@
 #include "dialog.h"
 
 #include <QDialogButtonBox>
+#include <QFormLayout>
+#include <QLabel>
 #include <QVBoxLayout>
+
+#include <qxsysinfo.h>
 
 
 Dialog::Dialog(QWidget* parent)
     : QDialog{parent}
 {
+    // System information
+
+    QLabel* buildQtVersion = new QLabel(QxSysInfo::buildQtVersion());
+
+    QFormLayout* layoutSysInfo = new QFormLayout;
+    layoutSysInfo->addRow(tr("Build Qt Version:"), buildQtVersion);
 
     // Button
 
@@ -25,9 +35,12 @@ Dialog::Dialog(QWidget* parent)
     //
 
     QVBoxLayout* layout = new QVBoxLayout;
+    layout->addLayout(layoutSysInfo);
     layout->addStretch();
     layout->addWidget(buttonBox);
     setLayout(layout);
+
+    layoutSysInfo->setHorizontalSpacing(layoutSysInfo->horizontalSpacing() * 2);
 
     resize(640, 480);
 }
